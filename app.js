437b5f74796ee5672ec02468fce4f257f763ca6e
Talib Hussain Khan <talibhk161@gmail.com>
2025-05-17 17:24:59 +0500

    update

--- app.js
+++ app.js
@@ -1,45 +1,41 @@
-let signUpUsername = document.getElementById("signUpUserName");
-let signUpEmail = document.getElementById("signUpEmail");
-let signUpPassword = document.getElementById("signUpPassword");
-let signInEmail = document.getElementById("signInEmail");
-let signInPassword = document.getElementById("signInPassword");
-let home = document.getElementById('homeH1');
+
+var userArr = JSON.parse(localStorage.getItem('users')) || [];
 
 
+function signUp(){
+ var signUpUsername = document.getElementById("signUpUserName");
+ var signUpEmail = document.getElementById("signUpEmail");
+ var signUpPassword = document.getElementById("signUpPassword");
 
-
-const signUp = ()=>{
-
-    let userName = signUpUsername.value;
-    let email = signUpEmail.value;
-    let password = signUpPassword.value;
-    if(userName === "" || email === "" || password === "" ){
+let user ={
+    userName : signUpUsername.value,
+    email : signUpEmail.value,
+    password : signUpPassword.value
+}
+     if(signUpUsername.value === "" || signUpEmail.value === "" || signUpPassword.value === "" ){
         alert('please fill the all field first')
     }else{
-        localStorage.setItem('user', JSON.stringify({userName, password, email}))
+       userArr.push(user)
+        localStorage.setItem('users', JSON.stringify(userArr))
         window.location.href = 'signInPage.html';
     }
 }
 
 const signIn =() =>{
 
-    let email = signInEmail.value;
-    let password = signInPassword.value;
+    let email = document.getElementById('signInEmail');
+    let password = document.getElementById('signInPassword');
+    let users = JSON.parse(localStorage.getItem('users'));
+    
+    let filterUser = users.filter(user => user.email === email.value && user.password === password.value )
 
-    let storeData = JSON.parse(localStorage.getItem('user'));
-
-    if(storeData && storeData.email === email && storeData.password === password){
-        // alert('login')
-        localStorage.setItem("isAuthentication", true)
+    if(filterUser.length){
+        localStorage.setItem('isAuthentication', true)
         window.location.href = 'home.html'
-        
     }else{
-        alert("Invalid email and password")
+        alert('invalid credentials')
     }
 }
-
-let data = JSON.parse(localStorage.getItem('user'))
-home.innerHTML = `Welcome, ${data.userName[0].toUpperCase() + data.userName.slice(1)}`
 
 const logout= ()=>{
     localStorage.removeItem('isAuthentication');
